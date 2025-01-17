6058fbe1cc251e14682b43e9a314c45d203c3792
dieftsx <diefersonfrontenddeveloper@gmail.com>
2025-01-16 23:02:49 -0300

    Chore: remoção da abreviação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,19 +34,19 @@ export default function Home() {
                   <svg className="w-5 h-5 text-amber-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                   </svg>
-                Avaliação Neuropsi
+                Avaliação Neuropsicológica
                 </li>
                 <li className="flex items-center">
                   <svg className="w-5 h-5 text-amber-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                   </svg>
-                  Reabilitação Neuropsi
+                  Reabilitação Neuropsicológica
                   </li>
                 <li className="flex items-center">
                   <svg className="w-5 h-5 text-amber-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
                   </svg>
-                  Neurofeedback
+                      Neurofeedback
                   </li>
                  </ul>
             </div>
